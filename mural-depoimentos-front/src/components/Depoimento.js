3b6dedbe16cc31c139fc6adf84542282d14a440a
marlon307 <marlon-ramosb@hotmail.com>
2021-12-03 17:22:43 -0300

    Component depoimento

---
+++ mural-depoimentos-front/src/components/Depoimento.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Depoimento({ img, userName, text }) {
+  return (
+    <section className="depoimento">
+      <div className="photo">
+        <img src={ img } alt="imageName" />
+      </div>
+      <p className="userName">User: { userName }</p>
+      <span className="text">
+        <p>{ text }</p>
+      </span>
+    </section>
+  )
+}
+
+export default Depoimento
