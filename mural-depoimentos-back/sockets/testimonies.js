5a6d0a9343d31a03726a3483f4cb9db111ea3e5b
marlon307 <marlon-ramosb@hotmail.com>
2021-12-04 15:43:40 -0300

    Update front send infos socket

--- mural-depoimentos-back/sockets/testimonies.js
+++ mural-depoimentos-back/sockets/testimonies.js
@@ -1,9 +1,10 @@
-const { createTestimony } = require('../models/testimonies')
+const { createTestimony, getAllTestimonies } = require('../models/testimonies')
 
 module.exports = (io) => {
   io.on('connection', (socket) => {
-    socket.on('createTestimony', async ({ userName, text }) => {
-      const testimonies = await createTestimony(userName, text);
+    socket.on('sendDepoiment', async ({ userName, testimonial }) => {
+      // console.log(userName, testimonial);
+      await createTestimony(userName, testimonial);
       const alltestimonies = await getAllTestimonies();
       io.emit('resTestimony', alltestimonies)
     })
